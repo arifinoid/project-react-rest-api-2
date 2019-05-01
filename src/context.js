a5707a1e504ad3eab8a0b535927dfad5dbaeca4c
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-05-01 15:32:19 +0700

    initial commit

---
+++ src/context.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+  state = {
+    track_list: [],
+    heading: "Top 10 Tracks"
+  };
+
+  componentDidMount() {
+    axios
+      .get()
+      .then(result => console.log(result.data))
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+export const Consumer = Context.Consumer;
